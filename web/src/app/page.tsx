e57a0f880490e5c7e0558c3a8134f4c71f068601
jepzec <mnaufalkadisatria13@gmail.com>
2025-05-18 12:28:21 +0700

    changes on HomePage

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,10 +1,11 @@
-import RegisterPage from "./auth/register/page";
-
 export default function HomePage() {
   return (
     <main>
-      <h1>Uhh these are he stuff</h1>
-      <RegisterPage />
+      <h1>Hello, this is update check</h1>
+      <p>
+        yeah, this is a Lorem ipsum, dolor sit amet consectetur adipisicing
+        elit. Excepturi, autem. Beatae unde neque aliquid sequi.
+      </p>
     </main>
   );
 }
